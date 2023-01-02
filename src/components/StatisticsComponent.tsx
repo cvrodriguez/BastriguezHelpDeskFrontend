b5619dabdcb2b4d55f74861679986dd88f83cc6d
cvrodriguez <cinvir15@gmail.com>
2023-01-02 16:14:05 +0100

    showing total tickets and total opened tickets

--- src/components/StatisticsComponent.tsx
+++ src/components/StatisticsComponent.tsx
@@ -1,6 +1,60 @@
 import * as React from "react";
+
+import { useEffect } from "react";
+
+import { useAppSelector, useAppDispatch } from '../hooks'
+import { selectTotalTickets, selectTotalTicketsOpened } from "../store/ticket/slectors";
+import { fetchTickets } from "../store/ticket/thunks";
+
+import '../style/statictics.css'
+import { VscListOrdered } from "react-icons/vsc";
+import { BsBarChartFill } from "react-icons/bs";
+
 export const StatisticsComponent: React.FC<{}> = () => {
-    return(
-        <div>StatisticsComponent</div>
+
+    const dispatch = useAppDispatch()
+    const totalTickets = useAppSelector(selectTotalTickets)
+    const totalTicketsOpen = useAppSelector(selectTotalTicketsOpened)
+
+    useEffect(() => {
+        dispatch(fetchTickets())
+    }, [])
+
+    return (
+        <div className="statictics-page">
+
+            <div className="total-tickets">
+                <div className='title-total-tickets'>
+                    <VscListOrdered className='TiTicket' />
+                    <h5>Total Tickets</h5>
+                </div>
+                <div className="total-opened-tickets">
+                    <span>{totalTicketsOpen.length} Opened</span>
+                </div>
+                <div className="number-of-total-tickets">{totalTickets}</div>
+                <div className="numbers-by-week">
+
+                    <span><BsBarChartFill />  this week</span>
+                    <span>this week</span>
+                </div>
+
+            </div>
+
+
+
+
+
+            <div className="total-tickets">
+                <div className='title-total-tickets'>
+                    <VscListOrdered className='TiTicket' />
+                    <h3>Total Tickets</h3>
+                </div>
+
+                <span>{} Opened</span>
+                {totalTickets}
+                <span>this week</span>
+            </div>
+
+        </div >
     )
 }
--- src/store/ticket/slectors.ts
+++ src/store/ticket/slectors.ts
@@ -1,3 +1,5 @@
 import type { RootState } from '../../store'
 export const selectTickets = (state: RootState) => state.tickets.ticketList
 export const selectTicketById = (state: RootState) => state.tickets.ticketDetail
+export const selectTotalTickets = (state: RootState) => state.tickets.ticketList.length
+export const selectTotalTicketsOpened = (state: RootState) => state.tickets.ticketList.filter((t)=> t.state === 'Open')
